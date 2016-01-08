9256bdf6664bd7be643afdfc56877f7d2147194a
Philip Schielke <philip.schielke@concordia.edu>
2016-01-08 14:42:29 -0600

    Cleanup

--- src/ruse/SymbolTable.java
+++ src/ruse/SymbolTable.java
@@ -1,10 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
 package ruse;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.util.HashMap;
 
 /**
@@ -12,33 +15,38 @@ import java.util.HashMap;
  * @author phil
  */
 public class SymbolTable {
-    HashMap<String,Object> table;
-    
+    HashMap<String, Object>   table;
     private final SymbolTable pred;
-    
-    public SymbolTable() { 
+
+    public SymbolTable() {
         table = new HashMap<>();
-        pred = null;
+        pred  = null;
     }
-    
+
     public SymbolTable(SymbolTable p) {
         table = new HashMap<>();
-        pred = p;
+        pred  = p;
     }
-    
-    public SymbolTable getPred() { return pred; }
-    
+
+    public SymbolTable getPred() {
+        return pred;
+    }
+
     public boolean enter(String s, Object entry) {
         Object value = lookup(s);
+
         table.put(s, entry);
-        return(value==null);
+
+        return (value == null);
     }
-    
+
     public Object lookup(String s) {
         Object value = table.get(s);
-        if (value == null && pred!=null)
+
+        if ((value == null) && (pred != null)) {
             value = pred.lookup(s);
+        }
+
         return value;
     }
 }
-
